9f73b3b664e5eb1d7ca46748b3d75939e2de07ce
Alvin Lawson <al11588@gmail.com>
2017-01-29 10:50:04 -0500

    fixing the last test

--- tests.py
+++ tests.py
@@ -11,14 +11,14 @@ class SeleniumTest(unittest.TestCase):
         self.browser.get('https://hub.docker.com/')
 
         #1.Search related test case
-    def testSearch(self):
+    def testNumber1(self):
     	#search field
         searchfield = self.browser.find_element_by_xpath("//div/main/div/div[1]/div/nav/section/ul[2]/li/div/form/div/input").send_keys("Flashpoint")
         #searchfield return
         searchfieldenter = self.browser.find_element_by_xpath("//div/main/div/div[1]/div/nav/section/ul[2]/li/div/form/div/input").send_keys(Keys.RETURN)
 
         #2.Signup related test case
-    def testSignup(self):
+    def testNumber2(self):
     	#docker ID field
     	dockeridfield = self.browser.find_element_by_xpath("//div/main/div/div[1]/header/div/div[2]/div/form/div[1]/div/input").send_keys("alvindockerid")
     	#email field
@@ -30,7 +30,7 @@ class SeleniumTest(unittest.TestCase):
         signupbutton.click()# signup button is clicked
 
         #3.One login related test case
-    def testThisOneLogin(self):
+    def testNumber3(self):
     	#login
     	login = self.browser.find_element_by_link_text("Sign in").click()
     	#username
@@ -40,8 +40,8 @@ class SeleniumTest(unittest.TestCase):
     	#submitbutton
     	submitbutton2 = self.browser.find_element_by_id("nw_submit").click()
 
-    	#clicks on explore, writes flashpoint on searchfield, presses back button, refreshes
-    def testThisIsComplex(self):
+    	#4. Clicks on explore, writes flashpoint on searchfield, presses back button, refreshes
+    def testNumber4(self):
     	#clicks on explore
     	explore = self.browser.find_element_by_link_text("Explore").click()
     	#searchfield
@@ -51,26 +51,23 @@ class SeleniumTest(unittest.TestCase):
     	#refreshes
     	refresh = self.browser.refresh()
 
+    	#5. Maximizes window, refreshes, verifies copyright text, 
+    def testNumber5(self):
+    	#maximize window
+    	maximizewindow = self.browser.maximize_window()
+    	#refresh
+    	refresh = self.browser.refresh()
+    	#checks 2016 docker inc
+    	checks = self.browser.find_element_by_class_name('Welcome__footerCopy___3AswA')
+    	#refreshes twice
+    	refresh = self.browser.refresh()
+    	#clicks help
+    	helpclick = self.browser.find_element_by_link_text("Help").click()
+
 
 
 
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=3)
-
-#DockerID xpath: //div/main/div/div[1]/header/div/div[2]/div/form/div[1]/div/input
-
-#Emailaddress xpath: //header[@class='Welcome__header___2oKOq']/div/div[2]/div/form/div[2]/div/input
-
-#password xpath: //header[@class='Welcome__header___2oKOq']/div/div[2]/div/form/div[3]/div/input
-
-#signup button xpath: //header[@class='Welcome__header___2oKOq']//button[.='Sign Up']
-
-#search bar xpath: //input[@class='SearchBar__searchInput___34nC3']
-
-#1.search related test case
-
-#2.signup related test case
-
-#3.one login related test case 
+    unittest.main(verbosity=3) 
