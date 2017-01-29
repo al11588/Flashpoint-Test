4b4c8cb4834d663f11c97fee454a8d7ad1baefdd
Alvin Lawson <al11588@gmail.com>
2017-01-29 07:17:07 -0500

    pushing good code

--- tests.py
+++ tests.py
@@ -1,33 +1,17 @@
-import os
-
+import unittest
 from selenium import webdriver
 
-chromedriver = "/Users/alvinlawson/Desktop/Alvin-Lawson---Flashpoint-Test/chromedriver"
+class SeleniumTest(unittest.TestCase):
 
-os.environ["webdriver.chrome.driver"] = chromedriver
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get('https://hub.docker.com/')
 
-driver = webdriver.Chrome(chromedriver)
-
-driver.get("https://hub.docker.com/")
-
-
-
+    def testSearch(self):
+        elem = self.browser.find_element_by_xpath("//header[@class='Welcome__header___2oKOq']//button[.='Sign Up']")
+        elem.click()
+        #elem.send_keys('selenium')
 
 
-
-#DockerID xpath: //div/main/div/div[1]/header/div/div[2]/div/form/div[1]/div/input
-
-#Emailaddress xpath: //header[@class='Welcome__header___2oKOq']/div/div[2]/div/form/div[2]/div/input
-
-#password xpath: //header[@class='Welcome__header___2oKOq']/div/div[2]/div/form/div[3]/div/input
-
-#signup button xpath: //header[@class='Welcome__header___2oKOq']//button[.='Sign Up']
-
-#search bar xpath: //input[@class='SearchBar__searchInput___34nC3']
-
-#1.search related test case
-
-#2.signup related test case
-
-#3.one login related test case 
-
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
